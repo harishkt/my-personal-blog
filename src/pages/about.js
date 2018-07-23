f9f78850a711a10a20af30079c7bc669b89ada22
hkumar <harisht@udel.edu>
2018-07-23 16:28:40 -0400

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,37 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
-
+const mainSection = css({
+	maxWidth: '50%'
+})
+const aboutSection = css({
+	display: 'flex',
+	flexWrap: 'wrap'
+});
+const skillsSection = css({
+	maxWidth: '50%',
+	paddingLeft: '40px',
+	marginLeft: '30px'
+});
 export default () => (
-	<Layout>About page</Layout>
+	<Layout>
+		<div className={aboutSection}>
+			<section className={mainSection}>
+				<h1>ABOUT</h1>
+				<p> Hello I'm Harish, Software Engineer. I intend to use this blog 
+					as a tracking tool for my learning process.
+				</p>
+			</section>
+			<aside className={skillsSection}>
+				<h1>Skills</h1>
+				<ul style={{listStyle: 'none'}}>
+					<li>Languages: JavaScript, Java, Ruby & Scala</li>
+					<li>Frameworks: React, Gatsby</li>
+					<li>Run time Environments: Nodejs</li>
+				</ul>
+			</aside>
+		</div>
+		
+		
+	</Layout>
 )
