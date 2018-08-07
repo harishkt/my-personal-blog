149d8788c798cf43ca480642df1ee4b166af9ae8
hkumar <harisht@udel.edu>
2018-08-07 13:42:37 -0400

    list categories page

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,31 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { css } from 'react-emotion';
-
-const headerLinks = css({
-  float: 'right',
-  paddingLeft: '10px',
-  paddingRight: '10px'
-});
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+	<div
+		style={{
+			marginBottom: '1.45rem',
+		}}
+	>
+		<div
+			style={{
+				margin: '0 auto',
+				maxWidth: 960,
+				padding: '1.45rem 1.0875rem',
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						textDecoration: 'none',
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</div>
+	</div>
 )
 
 export default Header
--- src/pages/category.js
+++ src/pages/category.js
@@ -1,49 +1,94 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql, Link } from 'gatsby';
+import styled from 'react-emotion';
+import Img from 'gatsby-image';
+
 import { capitalize } from '../utils/other-utils';
 
-export default () => {
+const CategoryListContainer = styled('div')`
+	display: flex;
+	width: 100%;
+	flex-flow: row wrap;
+`;
+
+const CategoryContainer = styled('div')`
+	margin-left: 20px;
+	margin-right: 20px;
+	width: 100%;
+	:hover {
+		width: 120%;
+		font-size: 120%;
+	}
+`;
+
+export default ({ data }) => {
+	const { edges } = data.allContentfulCategory;
+	const categoriesList = edges.map(({ node, index }) => {
+		const { fluid } = node.icon;
+		const { title } = node;
+		const { excerpt } = node.shortDescription.childMarkdownRemark;
+		// style={{ width: '40px', height: '40px'}}
+		return(
+			<Link to={`category/${title}`}>
+				<CategoryContainer key={index}>
+				<h3>{capitalize(title)}</h3>
+				<Img fluid={fluid}
+					alt={"Logo"}
+					fadeIn={true}
+					style={{ width: '300px', height: '300px'}}
+				/>
+				<p>{excerpt}</p>
+				</CategoryContainer>
+			</Link>
+			
+		)
+	})
 	return(
-		<Layout>Work in Progress</Layout>
+		<Layout>
+			<CategoryListContainer>
+				{categoriesList}
+			</CategoryListContainer>	
+		
+		</Layout>
 	)
 }
 
 export const query = graphql`
 	query {
-		allContentfulPost {
-			edges {
-				node {
-					category {
-						icon {
-							fluid {
-								src
-								srcSet
-								aspectRatio
-								sizes
-							}
-							fixed {
-								width
-								height
-								base64
-								src
-								srcSet
-							}
-						}
-						id
-						title
-					}
-					slug
-					title {
-						title
-					}
-					body {
-						childMarkdownRemark {
-							excerpt
-						}
+		allContentfulCategory {
+	  edges {
+		node {
+		  title
+		  icon {
+				fluid {
+					src
+					srcSet
+					aspectRatio
+					sizes
+				}
+				fixed {
+					width
+					height
+					base64
+					src
+					srcSet
 					}
 				}
+			id
+			title
+		  post {
+			id
+		  }
+		  shortDescription {
+			id
+			childMarkdownRemark {
+				excerpt
+				html
+			}
 			}
 		}
+		}
 	}
+}
 `;
